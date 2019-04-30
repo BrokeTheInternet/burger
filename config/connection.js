384761dedd72280951cb1d086bc14d27e8effcb0
iBrokeTheInternet <brandonotero14@gmail.com>
2019-04-29 21:46:19 -0400

    Stress Fix

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "5361869LoL",
+    database: "burgers_db"
+  });
+}
+connection.connect();
+module.exports = connection;
